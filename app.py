6354d7be3d07b5b528d680fbf05d501aeb230c90
Felix Ward <22373561+FlexDW@users.noreply.github.com>
2021-11-09 16:26:01 +1100

    heroku setup

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import os
 import json
-import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table
 from dash.dependencies import Input, Output, State, MATCH, ALL
 from dash.exceptions import PreventUpdate
 from layouts import tiers, results
@@ -32,9 +30,4 @@ results.register_callbacks(app)
 
 
 if __name__ == '__main__':
-    app.run_server(
-        port=3539, 
-        host='0.0.0.0', 
-        debug=True, 
-        dev_tools_silence_routes_logging=False
-    )
+    app.run_server()
--- dockerfile
+++
@@ -1,8 +0,0 @@
-FROM continuumio/anaconda3:2021.05
-RUN apt-get -y update --fix-missing &&\
-  apt-get -y install python3-pip &&\
-  pip install dash==1.21.0
-COPY . /40draw5
-WORKDIR /40draw5
-ENTRYPOINT ["python", "app.py"]
-
